ff24556ad6210c150615cdf887ea992418e2bb7a
imanavaz <i_avazpour@yahoo.com>
2015-12-15 14:01:27 +1100

    Working on drawing canvas

---
+++ Trajectory_Drawing/Scripts/canvasdrawing.js
@@ -0,0 +1,31 @@
+﻿
+function resourceLoaded() {
+    if (++curLoadResNum >= totalLoadResources) {
+        redraw();
+    }
+}
+
+function loadCanvas() {
+
+    // Create the canvas (Neccessary for IE because it doesn't know what a canvas element is)
+    var canvasDiv = document.getElementById('mycanvas');
+    canvas = document.createElement('canvas');
+    canvas.setAttribute('width', canvasWidth);
+    canvas.setAttribute('height', canvasHeight);
+    canvas.setAttribute('id', 'canvas');
+    canvasDiv.appendChild(canvas);
+    if (typeof G_vmlCanvasManager != 'undefined') {
+        canvas = G_vmlCanvasManager.initElement(canvas);
+    }
+    context = canvas.getContext("2d");
+
+    trajectoryImage.onload = function () {
+        resourceLoaded();
+    };
+    trajectoryImage.src = "Images/ExperimentMap.png";
+
+
+
+
+
+}
