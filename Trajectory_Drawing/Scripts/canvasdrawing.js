ec7db1d5a370dbe15ee9199da07501cca0d0eb80
imanavaz <i_avazpour@yahoo.com>
2015-12-16 10:34:04 +1100

    Canvas colors working, but still not complete

--- Trajectory_Drawing/Scripts/canvasdrawing.js
+++ Trajectory_Drawing/Scripts/canvasdrawing.js
@@ -1,62 +1,18 @@
 ﻿var canvas;
-var context;
-var canvasWidth = 1000;
-var canvasHeight = 600;
-var padding = 25;
-var lineWidth = 8;
-var colorPurple = "#cb3594";
-var colorGreen = "#659b41";
-var colorYellow = "#ffcf33";
-var colorBrown = "#986928";
+var ctx;
+var flag = false;
+var prevX = 0,
+    currX = 0,
+    prevY = 0,
+    currY = 0,
+    dot_flag = false;
 var trajectoryImage = new Image();
-var outlineImage = new Image();
-var crayonImage = new Image();
-var markerImage = new Image();
-var eraserImage = new Image();
-var crayonBackgroundImage = new Image();
-var markerBackgroundImage = new Image();
-var eraserBackgroundImage = new Image();
-var crayonTextureImage = new Image();
-var clickX = new Array();
-var clickY = new Array();
-var clickColor = new Array();
-var clickTool = new Array();
-var clickSize = new Array();
-var clickDrag = new Array();
-var paint = false;
-var curColor = colorPurple;
-var curTool = "marker";
-var curSize = "normal";
-var mediumStartX = 18;
-var mediumStartY = 19;
-var mediumImageWidth = 93;
-var mediumImageHeight = 46;
-var drawingAreaX = 111;
-var drawingAreaY = 11;
-var drawingAreaWidth = 700;
-var drawingAreaHeight = 400;
-var toolHotspotStartY = 23;
-var toolHotspotHeight = 38;
-var sizeHotspotStartY = 157;
-var sizeHotspotHeight = 36;
-var sizeHotspotWidthObject = new Object();
-sizeHotspotWidthObject.huge = 39;
-sizeHotspotWidthObject.large = 25;
-sizeHotspotWidthObject.normal = 18;
-sizeHotspotWidthObject.small = 16;
-var totalLoadResources = 1;///////Iman
-var curLoadResNum = 0;
+var canvasWidth = 1000;
+var canvasHeight = 800;
+var currColor;
 
-
-function resourceLoaded() {
-    if (++curLoadResNum >= totalLoadResources) {
-        redraw();
-    }
-}
 
 function loadCanvas() {
-
-    // Create the canvas (Neccessary for IE because it doesn't know what a canvas element is)
     var canvasDiv = document.getElementById('canvasDiv');
     canvas = document.createElement('canvas');
     canvas.setAttribute('width', canvasWidth);
@@ -66,288 +22,88 @@ function loadCanvas() {
     if (typeof G_vmlCanvasManager != 'undefined') {
         canvas = G_vmlCanvasManager.initElement(canvas);
     }
-    context = canvas.getContext("2d");
 
-    trajectoryImage.onload = function () {
-        resourceLoaded();
-    };
+    ctx = canvas.getContext("2d");
+    w = canvas.width;
+    h = canvas.height;
+
+    canvas.addEventListener("mousemove", function (e) {
+        findxy('move', e)
+    }, false);
+    canvas.addEventListener("mousedown", function (e) {
+        findxy('down', e)
+    }, false);
+    canvas.addEventListener("mouseup", function (e) {
+        findxy('up', e)
+    }, false);
+    canvas.addEventListener("mouseout", function (e) {
+        findxy('out', e)
+    }, false);
+
     trajectoryImage.src = "Images/ExperimentMap.png";
 
+}
 
-
-    //Mouse events
-    $('#canvas').mousedown(function(e)
-    {
-        // Mouse down location
-        var mouseX = e.pageX - this.offsetLeft;
-        var mouseY = e.pageY - this.offsetTop;
-		
-        if(mouseX < drawingAreaX) // Left of the drawing area
-        {
-            if(mouseX > mediumStartX)
-            {
-                if(mouseY > mediumStartY && mouseY < mediumStartY + mediumImageHeight){
-                    curColor = colorPurple;
-                }else if(mouseY > mediumStartY + mediumImageHeight && mouseY < mediumStartY + mediumImageHeight * 2){
-                    curColor = colorGreen;
-                }else if(mouseY > mediumStartY + mediumImageHeight * 2 && mouseY < mediumStartY + mediumImageHeight * 3){
-                    curColor = colorYellow;
-                }else if(mouseY > mediumStartY + mediumImageHeight * 3 && mouseY < mediumStartY + mediumImageHeight * 4){
-                    curColor = colorBrown;
-                }
-            }
-        }
-        else if(mouseX > drawingAreaX + drawingAreaWidth) // Right of the drawing area
-        {
-            if(mouseY > toolHotspotStartY)
-            {
-                if(mouseY > sizeHotspotStartY)
-                {
-                    var sizeHotspotStartX = drawingAreaX + drawingAreaWidth;
-                    if(mouseY < sizeHotspotStartY + sizeHotspotHeight && mouseX > sizeHotspotStartX)
-                    {
-                        if(mouseX < sizeHotspotStartX + sizeHotspotWidthObject.huge){
-                            curSize = "huge";
-                        }else if(mouseX < sizeHotspotStartX + sizeHotspotWidthObject.large + sizeHotspotWidthObject.huge){
-                            curSize = "large";
-                        }else if(mouseX < sizeHotspotStartX + sizeHotspotWidthObject.normal + sizeHotspotWidthObject.large + sizeHotspotWidthObject.huge){
-                            curSize = "normal";
-                        }else if(mouseX < sizeHotspotStartX + sizeHotspotWidthObject.small + sizeHotspotWidthObject.normal + sizeHotspotWidthObject.large + sizeHotspotWidthObject.huge){
-                            curSize = "small";						
-                        }
-                    }
-                }
-                else
-                {
-                    //if(mouseY < toolHotspotStartY + toolHotspotHeight){
-                    //    curTool = "crayon";
-                    //} else
-                    //if (mouseY < toolHotspotStartY + toolHotspotHeight * 2) {
-                        curTool = "marker";
-                    //}else if(mouseY < toolHotspotStartY + toolHotspotHeight * 3){
-                      //  curTool = "eraser";
-                    //}
-                }
-            }
-        }
-        else if(mouseY > drawingAreaY && mouseY < drawingAreaY + drawingAreaHeight)
-        {
-            // Mouse click location on drawing area
-        }
-
-        curTool = "marker";//////
-        paint = true;
-        addClick(mouseX, mouseY, false);
-        redraw();
-    });
-	
-    $('#canvas').mousemove(function(e){
-        if(paint==true){
-            addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
-            redraw();
-        }
-    });
-	
-    $('#canvas').mouseup(function(e){
-        paint = false;
-        redraw();
-    });
-	
-    $('#canvas').mouseleave(function(e){
-        paint = false;
-    });
+function draw() {
+    ctx.beginPath();
+    ctx.moveTo(prevX, prevY);
+    ctx.lineTo(currX, currY);
+    ctx.strokeStyle = currColor;
+    ctx.lineWidth = 4;
+    ctx.stroke();
+    ctx.closePath();
 }
 
 
-/**
-* Adds a point to the drawing array.
-* @param x
-* @param y
-* @param dragging
-*/
-function addClick(x, y, dragging) {
-    clickX.push(x);
-    clickY.push(y);
-    clickTool.push(curTool);
-    clickColor.push(curColor);
-    clickSize.push(curSize);
-    clickDrag.push(dragging);
+function erase() {
+    var m = confirm("Want to clear");
+    if (m) {
+        ctx.clearRect(0, 0, w, h);
+        document.getElementById("canvasimg").style.display = "none";
+    }
+}
+
+function save() {
+    document.getElementById("canvasimg").style.border = "2px solid";
+    var dataURL = canvas.toDataURL();
+    document.getElementById("canvasimg").src = dataURL;
+    document.getElementById("canvasimg").style.display = "inline";
+}
+
+function findxy(res, e) {
+    if (res == 'down') {
+        prevX = currX;
+        prevY = currY;
+        currX = e.clientX - canvas.offsetLeft;
+        currY = e.clientY - canvas.offsetTop;
+
+        flag = true;
+        dot_flag = true;
+        if (dot_flag) {
+            ctx.beginPath();
+            ctx.fillStyle = currColor;
+            ctx.fillRect(currX, currY, 2, 2);
+            ctx.closePath();
+            dot_flag = false;
+        }
+    }
+    if (res == 'up' || res == "out") {
+        flag = false;
+    }
+    if (res == 'move') {
+        if (flag) {
+            prevX = currX;
+            prevY = currY;
+            currX = e.clientX - canvas.offsetLeft;
+            currY = e.clientY - canvas.offsetTop;
+            draw();
+        }
+    }
 }
 
 
-/**
-* Clears the canvas.
-*/
-function clearCanvas() {
-    context.clearRect(0, 0, canvasWidth, canvasHeight);
-    // Draw the outline image
-    //context.drawImage(trajectoryImage, drawingAreaX, drawingAreaY, drawingAreaWidth, drawingAreaHeight);
+function update(picker) {
+    currColor = picker.toHEXString();
 }
-
-
-
-/**
-* Redraws the canvas.
-*/
-function redraw() {
-    // Make sure required resources are loaded before redrawing
-    if (curLoadResNum < totalLoadResources) { return; }
-
-    clearCanvas();
-
-    var locX;
-    var locY;
-    //if (curTool == "marker") {
-        // Draw the marker tool background
-        //context.drawImage(markerBackgroundImage, 0, 0, canvasWidth, canvasHeight);
-
-        // Purple
-        //locX = (curColor == colorPurple) ? 18 : 52;
-        //locY = 19;
-
-        //context.beginPath();
-        //context.moveTo(locX + 10, locY + 24);
-        /*context.lineTo(locX + 10, locY + 24);
-        context.lineTo(locX + 22, locY + 16);
-        context.lineTo(locX + 22, locY + 31);
-        context.closePath();
-        context.fillStyle = colorPurple;
-        context.fill();
-
-        if (curColor == colorPurple) {
-            context.drawImage(markerImage, locX, locY, mediumImageWidth, mediumImageHeight);
-        } else {
-            context.drawImage(markerImage, 0, 0, 59, mediumImageHeight, locX, locY, 59, mediumImageHeight);
-        }
-
-        // Green
-        locX = (curColor == colorGreen) ? 18 : 52;
-        locY += 46;
-
-        context.beginPath();
-        context.moveTo(locX + 10, locY + 24);
-        context.lineTo(locX + 10, locY + 24);
-        context.lineTo(locX + 22, locY + 16);
-        context.lineTo(locX + 22, locY + 31);
-        context.closePath();
-        context.fillStyle = colorGreen;
-        context.fill();
-
-        if (curColor == colorGreen) {
-            context.drawImage(markerImage, locX, locY, mediumImageWidth, mediumImageHeight);
-        } else {
-            context.drawImage(markerImage, 0, 0, 59, mediumImageHeight, locX, locY, 59, mediumImageHeight);
-        }
-
-        // Yellow
-        locX = (curColor == colorYellow) ? 18 : 52;
-        locY += 46;
-
-        context.beginPath();
-        context.moveTo(locX + 10, locY + 24);
-        context.lineTo(locX + 10, locY + 24);
-        context.lineTo(locX + 22, locY + 16);
-        context.lineTo(locX + 22, locY + 31);
-        context.closePath();
-        context.fillStyle = colorYellow;
-        context.fill();
-
-        if (curColor == colorYellow) {
-            context.drawImage(markerImage, locX, locY, mediumImageWidth, mediumImageHeight);
-        } else {
-            context.drawImage(markerImage, 0, 0, 59, mediumImageHeight, locX, locY, 59, mediumImageHeight);
-        }
-
-        // Yellow
-        locX = (curColor == colorBrown) ? 18 : 52;
-        locY += 46;
-
-        context.beginPath();
-        context.moveTo(locX + 10, locY + 24);
-        context.lineTo(locX + 10, locY + 24);
-        context.lineTo(locX + 22, locY + 16);
-        context.lineTo(locX + 22, locY + 31);
-        context.closePath();
-        context.fillStyle = colorBrown;
-        context.fill();
-
-        if (curColor == colorBrown) {
-            context.drawImage(markerImage, locX, locY, mediumImageWidth, mediumImageHeight);
-        } else {
-            context.drawImage(markerImage, 0, 0, 59, mediumImageHeight, locX, locY, 59, mediumImageHeight);
-        }
-    }
-    else if (curTool == "eraser") {
-        context.drawImage(eraserBackgroundImage, 0, 0, canvasWidth, canvasHeight);
-        context.drawImage(eraserImage, 18, 19, mediumImageWidth, mediumImageHeight);
-    } else {
-        alert("Error: Current Tool is undefined");
-    }
-    */
-    if (curSize == "small") {
-        locX = 467;
-    } else if (curSize == "normal") {
-        locX = 450;
-    } else if (curSize == "large") {
-        locX = 428;
-    } else if (curSize == "huge") {
-        locX = 399;
-    }
-    locY = 189;
-    context.beginPath();
-    context.rect(locX, locY, 2, 12);
-    context.closePath();
-    context.fillStyle = '#333333';
-    context.fill();
-
-    // Keep the drawing in the drawing area
-    context.save();
-    context.beginPath();
-    context.rect(drawingAreaX, drawingAreaY, drawingAreaWidth, drawingAreaHeight);
-    context.clip();
-
-    var radius;
-    var i = 0;
-    for (; i < clickX.length; i++) {
-        if (clickSize[i] == "small") {
-            radius = 2;
-        } else if (clickSize[i] == "normal") {
-            radius = 5;
-        } else if (clickSize[i] == "large") {
-            radius = 10;
-        } else if (clickSize[i] == "huge") {
-            radius = 20;
-        } else {
-            alert("Error: Radius is zero for click " + i);
-            radius = 0;
-        }
-
-        context.beginPath();
-        if (clickDrag[i] && i) {
-            context.moveTo(clickX[i - 1], clickY[i - 1]);
-        } else {
-            context.moveTo(clickX[i], clickY[i]);
-        }
-        context.lineTo(clickX[i], clickY[i]);
-        context.closePath();
-
-        if (clickTool[i] == "eraser") {
-            //context.globalCompositeOperation = "destination-out"; // To erase instead of draw over with white
-            context.strokeStyle = 'white';
-        } else {
-            //context.globalCompositeOperation = "source-over";	// To erase instead of draw over with white
-            context.strokeStyle = clickColor[i];
-        }
-        context.lineJoin = "round";
-        context.lineWidth = radius;
-        context.stroke();
-
-    }
-    //context.globalCompositeOperation = "source-over";// To erase instead of draw over with white
-    context.restore();
 
     
-    context.globalAlpha = 1; // No IE support
-
-    
-}
